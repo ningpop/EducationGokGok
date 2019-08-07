1c458a1e6ce7f1bf44f99cc4dd7814784e3695b2
ningpop Lee <craft1933@naver.com>
2019-08-08 03:17:04 +0900

    commit before migrate accounts class

--- educationgokgok/board/urls.py
+++ educationgokgok/board/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
 
+#미디어파일 업로드를 위해 추가_현지원
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('new/', views.new, name='new'),
+    # path('create/', views.create, name='create'),
     path('detail/', views.detail, name='detail'),
+    # path('detail/<int:post_id>', views.detail, name='detail'),
     path('main/', views.board_main, name='board_main'),
     path('major/', views.board_major, name='board_major'),
     path('local/', views.board_local, name='board_local'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #미디어 파일 업로드
--- educationgokgok/board/views.py
+++ educationgokgok/board/views.py
@@ -1,18 +1,67 @@
 from django.shortcuts import render
+# from django.shortcuts import get_object_or_404, redirect
+# from .models import AAA (AAA: board앱 내 models.py의 class명)
+# from django.utils import timezone
+# from django.core.paginator import Paginator
 
 # Create your views here.
-
+'''
+* 키워드 단어 정리 *
+board : 강의 글들의 게시판
+post : 하나의 강의 글
+post_detail : 각각 강의 글의 detail
+post_list : 강의 글들의 게시판보다는 작은 개념, 글 들의 목록을 뜻함
+'''
 def new(request):
     return render(request, 'new.html')
 
+'''
+-> 글쓰기 후 form action으로 실행될 create 함수
+def create(request):
+    post = AAA()
+    post.title = request.GET['title']
+    post.body = request.GET['body']
+    post.pub_date = timezone.datetime.now()
+    post.save()
+    return redirect('/' + str(post.id))
+'''
+
 def detail(request):
     return render(request, 'detail.html')
+'''
+-> 추후 대체될 detail 함수
+def detail(request, post_id):
+    post_detail = get_object_or_404(AAA, pk=post_id)
+    return render(request, 'detail.html', {'post':post_detail})
+'''
+
 
 def board_main(request):
+    '''
+    post_list = AAA.objects.all()
+    paginator = Paginator(post_list, 10)
+    page = request.GET.get('page')
+    board = paginator.get_page(page)
+    return render(request, 'board_main.html', {'board':board})
+    '''
     return render(request, 'board_main.html')
 
 def board_major(request):
+    '''
+    post_list = AAA.objects.all()
+    paginator = Paginator(post_list, 10)
+    page = request.GET.get('page')
+    board = paginator.get_page(page)
+    return render(request, 'board_main.html', {'board':board})
+    '''
     return render(request, 'board_major.html')
 
 def board_local(request):
+    '''
+    post_list = AAA.objects.all()
+    paginator = Paginator(post_list, 10)
+    page = request.GET.get('page')
+    board = paginator.get_page(page)
+    return render(request, 'board_main.html', {'board':board})
+    '''
     return render(request, 'board_local.html')
