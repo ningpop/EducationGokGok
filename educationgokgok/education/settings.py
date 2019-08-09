c22c794da59d8b9e992a123a23a7525322e5ff7b
ningpop Lee <craft1933@naver.com>
2019-08-10 06:22:39 +0900

    real final commit

--- educationgokgok/education/settings.py
+++ educationgokgok/education/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p#brm7a8y%ia1y*g^=546@rjl+2ovo1lyg_g(!)pwy75+h9o7^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = = bool( os.environ.get('DJANGO_DEBUG', False) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
