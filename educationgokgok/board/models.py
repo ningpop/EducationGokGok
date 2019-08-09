47e0fb8c265491fea96c4ef7dae9a92b50e4ebf4
ningpop Lee <craft1933@naver.com>
2019-08-09 11:19:10 +0900

    [DB]_board/models.py add class

--- educationgokgok/board/models.py
+++ educationgokgok/board/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+''' 
+*멘토 클래스*
+- 회원 기본 정보(Info)클래스 상속 
+- 사진(Image)
+- 분야(Major)
+- 지역(Area)
+- 소개 글(description)
+- 카톡 오픈 채팅 주소(Contact)
+'''
+class Mento(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=225)
+    major = models.CharField(max_length=10)
+    area = models.CharField(max_length=10)
+    body = models.TextField()
+    contact = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
